62b406f90eacc03ed415b2e2df7b159effc79f99
mulleramaral <miilleramaral@gmail.com>
2017-10-28 19:57:26 -0200

    Template Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    { path: 'templateForm', component: TemplateFormComponent },
+    { path: 'dataForm', component: DataFormComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'templateForm' }
 ];
 
 @NgModule({
