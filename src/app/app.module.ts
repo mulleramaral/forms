062239df289ac5c2c050b2069a77976cea496e2d
mulleramaral <miilleramaral@gmail.com>
2017-11-16 23:10:47 -0200

    Data form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { TemplateFormModule } from './template-form/template-form.module';
 
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { FormDebugComponent } from './form-debug/form-debug.component';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    DataFormModule,
     TemplateFormModule,
     AppRoutingModule
   ],
